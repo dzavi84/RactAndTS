e12f5607a3f7af0aa4a82428a2e99a464b4611f7
dzavi84 <milanvidanovic@yahoo.com>
2022-03-10 22:50:52 +0100

    commit99

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
-
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHendler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
-    <div className="App">
-      {/* A component that adds todos */}
-      <TodoList items={todos} />
+    <div className='App'>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHendler} />
     </div>
   );
 };
